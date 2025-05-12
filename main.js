ecc430a903fb72560f96e06fd3d7e2879c0e2df4
dimasyoga42 <dimasyoga9090@gmail.com>
2025-05-12 22:29:19 +0700

    add module helmet and joi

--- main.js
+++ main.js
@@ -7,12 +7,15 @@ import serverless from "serverless-http";
 import cors from "cors"
 import topup from "./src/routes/topup.routes.js";
 import admin from "./src/routes/admin.route.js";
+import helmet from "helmet";
 dotenv.config();
 const app  = express();
 const port = 3000
+
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors())
+app.use(helmet())
 
 app.use("/api", user)
 app.use("/api/store", topup)
